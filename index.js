4556a49cd8ee7f20ade85a7b8017ec1290629c05
Kari Ferenczy <klferenczy@gmail.com>
2019-01-26 18:27:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = new Object();
 
-function updateObjectWithKeyAndValue(recipes, key, value){
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
   recipes[key] = value;
   return recipes;
 }
