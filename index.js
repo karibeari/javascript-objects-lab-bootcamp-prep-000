024cba162e488e14f135b3cbf82ec3474349d9e0
Kari Ferenczy <klferenczy@gmail.com>
2019-01-26 18:19:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(recipes, key, value){
-  Object.assign({}, recipes, {[key]:value})
-  return recipes;
+  Object.assign(newRecipes, recipes, {[key]:value})
+  return newRecipes;
 }
